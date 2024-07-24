7b9d4b725a28855507c598b4087112b39824405b
Ash Manning <ash@layertwolabs.com>
2024-07-24 19:04:18 +0800

    refactor, fix minor bugs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ mod bip300;
 mod server;
 mod types;
 
+use futures::{
+    future::{self, Either},
+    FutureExt, TryFutureExt,
+};
 use miette::{miette, IntoDiagnostic, Result};
 use server::{validator::validator_server::ValidatorServer, Bip300};
 use tonic::transport::Server;
@@ -29,16 +33,29 @@ fn _create_client(main_datadir: &Path) -> Result<Client> {
     })
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let bip300 = Bip300::new(Path::new("./"))?;
-    bip300.run().await;
-    let addr: SocketAddr = "[::1]:50051".parse().into_diagnostic()?;
+async fn run_server(bip300: Bip300, addr: SocketAddr) -> Result<()> {
     println!("Listening for gRPC on {addr}");
     Server::builder()
         .add_service(ValidatorServer::new(bip300))
         .serve(addr)
+        .map(|res| res.into_diagnostic())
         .await
-        .into_diagnostic()?;
-    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let bip300 = Bip300::new(Path::new("./"))?;
+    let task = bip300
+        .run()
+        .map(|res| res.into_diagnostic())
+        .unwrap_or_else(|err| eprintln!("{err:#}"));
+    let addr: SocketAddr = "[::1]:50051".parse().into_diagnostic()?;
+    //let ((), ()) = future::try_join(task.map(Ok), run_server(bip300, addr)).await?;
+    match future::select(task, run_server(bip300, addr).boxed()).await {
+        Either::Left(((), server_task)) => {
+            // continue to run server task
+            server_task.await
+        }
+        Either::Right((res, _task)) => res,
+    }
 }
