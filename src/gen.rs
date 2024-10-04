be2960091aa92e1e7fe813d728e2f79c8661cfb4
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2024-10-04 17:44:19 -0400

    Add mainchain.proto definition

--- src/gen.rs
+++ src/gen.rs
@@ -2,6 +2,10 @@ pub mod validator {
     tonic::include_proto!("validator.v1");
 }
 
+pub mod mainchain {
+    tonic::include_proto!("mainchain.v1");
+}
+
 pub mod sidechain {
     tonic::include_proto!("sidechain");
 }
