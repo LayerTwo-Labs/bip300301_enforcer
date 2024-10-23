fb9c378a7b96302cd87ec15eabfe5f90fbe1d9e7
Torkel Rogstad <torkel@rogstad.io>
2024-10-23 22:02:40 +0200

    multi: mining improvements
    1. Give a clear error message that we cannot generate blocks on a
       non-regtest network
    2. Add better error messages to the mining process

--- src/server.rs
+++ src/server.rs
@@ -661,6 +661,13 @@ impl WalletService for Arc<crate::wallet::Wallet> {
         &self,
         request: tonic::Request<GenerateBlocksRequest>,
     ) -> std::result::Result<tonic::Response<GenerateBlocksResponse>, tonic::Status> {
+        // If we're not on regtest, this won't work!
+        if self.validator().network() != bitcoin::Network::Regtest {
+            return Err(tonic::Status::failed_precondition(
+                "can only generate blocks on regtest",
+            ));
+        }
+
         // FIXME: Remove `optional` from the `blocks` parameter in the proto file.
         let count = request.into_inner().blocks.unwrap_or(1);
         self.generate(count)
