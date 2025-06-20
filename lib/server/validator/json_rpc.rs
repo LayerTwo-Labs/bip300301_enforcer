fd8944f4d799efed218897565d7a2ce18102fab4
Roberto Santacroce <robertosantacrocemartins@gmail.com>
2025-06-20 19:50:09 +0200

    added json rpc for ctip

--- lib/server/validator/json_rpc.rs
+++ lib/server/validator/json_rpc.rs
@@ -1,5 +1,7 @@
-use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use jsonrpsee::{core::RpcResult, proc_macros::rpc, types::ErrorObject};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+use crate::types::{Ctip, SidechainNumber};
 
 #[derive(Clone, Copy, Debug)]
 pub struct Pong;
@@ -31,10 +33,18 @@ impl Serialize for Pong {
 pub trait Rpc {
     #[method(name = "ping")]
     fn ping(&self) -> RpcResult<Pong>;
+
+    #[method(name = "ctip")]
+    fn get_ctip(&self, sidechain_number: SidechainNumber) -> RpcResult<Ctip>;
 }
 
 impl RpcServer for crate::validator::Validator {
     fn ping(&self) -> RpcResult<Pong> {
         Ok(Pong)
+    }
+
+    fn get_ctip(&self, sidechain_number: SidechainNumber) -> RpcResult<Ctip> {
+        self.get_ctip(sidechain_number)
+            .map_err(|e| ErrorObject::owned(-1, e.to_string(), Option::<()>::None))
     }
 }
