8bd506516d4f5765ceec7208b4d8bfe104d2f78a
Torkel Rogstad <torkel@rogstad.io>
2025-07-08 15:56:53 +0200

    validator: adjust logs

--- lib/validator/task/mod.rs
+++ lib/validator/task/mod.rs
@@ -742,7 +742,7 @@ pub(in crate::validator) fn connect_block(
     let cumulative_work = dbs.block_hashes.cumulative_work().get(rwtxn, &block_hash)?;
     if Some(cumulative_work) > current_tip_cumulative_work {
         dbs.current_chain_tip.put(rwtxn, &(), &block_hash)?;
-        tracing::debug!("updated current chain tip: {}", height);
+        tracing::trace!("updated current chain tip: {}", height);
     }
     let event = {
         let header_info = HeaderInfo {
