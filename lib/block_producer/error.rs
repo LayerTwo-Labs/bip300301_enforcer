d0517c9f6039e2f2278f70add92d2e6c9bd1a527
/dev/fd0 <alicexbt@protonmail.com>
2026-08-13 11:01:19 +0200

    block producer: return mined block when BMM cleanup fails
    Reimplement #480 as an API-correctness fix on the current block producer.

--- lib/block_producer/error.rs
+++ lib/block_producer/error.rs
@@ -444,8 +444,6 @@ impl ToStatus for GenerateSignetBlock {
 pub enum GenerateBlock {
     #[error(transparent)]
     CoinbaseBuilder(#[from] CoinbaseMessagesError),
-    #[error("failed to delete BMM requests")]
-    DeleteBmmRequests(#[source] rusqlite::Error),
     #[error(transparent)]
     GenerateCoinbaseTxouts(#[from] GenerateCoinbaseTxouts),
     #[error(transparent)]
@@ -471,9 +469,7 @@ impl ToStatus for GenerateBlock {
             Self::Mine(err) => err.builder(),
             Self::SelectBlockTxs(err) => err.builder(),
             Self::TryGetMainchainTip(err) => err.builder(),
-            Self::DeleteBmmRequests(_) | Self::PushBytesBuf(_) | Self::ValidatorNotSynced => {
-                StatusBuilder::new(self)
-            }
+            Self::PushBytesBuf(_) | Self::ValidatorNotSynced => StatusBuilder::new(self),
         }
     }
 }
